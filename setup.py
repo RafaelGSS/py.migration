00b41a5d0d1b38efbd424138e3f63d2ab8632525
Rafael Gonzaga <rafael.nunu@hotmail.com>
2018-09-04 19:01:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ REQUIRED = [
 ]
 
 DEPENDENCY = [
-  'https://github.com/RafaelGSS/pymysql_wrapper#egg=pymysql_wrapper'   
+  'https://github.com/RafaelGSS/pymysql_wrapper/tarball/master#egg=pymysql_wrapper'   
 ]
 
 # What packages are optional?
